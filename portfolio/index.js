ffea076a098e5c8d85263eed1caae964d9ae04ea
meriem elhaouari <meriemelhaouari@outlook.com>
2024-12-14 12:20:47 +0100

    for deployment

---
+++ portfolio/index.js
@@ -0,0 +1,17 @@
+
+const button = document.getElementById('menubut');
+
+button.addEventListener('click', function() {
+  toggleMenu();
+
+
+
+function toggleMenu()  {
+    const menu = document.getElementById('menu');
+    
+    if (menu.style.display === "block") {
+     menu.style.display = "none"  
+    } else {menu.style.display = "block"
+    }}
+})
+    
